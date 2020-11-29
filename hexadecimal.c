118c83bcd65838399ebfd2aa6055233885f82ee9
Salas Souza <sal.souzasa@gmail.com>
2020-11-29 17:14:08 -0300

    fix

--- hexadecimal.c
+++ hexadecimal.c
@@ -3,6 +3,7 @@
 
 int main() {
 
+    /* unsigned --> interval without negative values */
   const unsigned int arraySize = 100;
 
   int * const array = (int *) malloc(sizeof(int) * arraySize);
@@ -10,7 +11,7 @@ int main() {
   for(unsigned int index = 0; index < arraySize; index++) {
       
     array[index] = rand();
-    
+
     printf("Integer %i is located at address 0x%x\n", array[index], &array[index]);
   }
   
